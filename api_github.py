a10b719b36d911bf7b42c7409a1d4d8af80cca0d
MatheusZoreck <matheuszoreck@gmail.com>
2025-09-30 18:14:03 -0300

    Adicionei script API GitHub

---
+++ api_github.py
@@ -0,0 +1,12 @@
+import requests
+
+
+url = "https://api.github.com/repos/torvalds/linux"
+
+resp = requests.get(url)
+
+data = resp.json()
+
+print("Repositório:", data["name"])
+print("Descrição:", data["description"])
+print("Número de estrelas:", data["stargazers_count"])
